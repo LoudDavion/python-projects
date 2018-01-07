63c60c4dcbfe4389542cccf274d2354a59914c92
Tony Jarjour <singdavion@gmail.com>
2018-01-07 05:44:15 -0500

    i did it

--- todolist.py
+++ todolist.py
@@ -1,1 +1,33 @@
-print('Your To Do List:')
+import sys
+
+
+
+
+def add(file, item):
+    file.write(item[4:] + '\n')
+    file.seek(0)
+    print(file.read())
+
+def rm(file, n):
+    if not (file.read())[3:]:
+        print('Nothing to do!')
+        
+
+
+with open('usertodo.txt', 'w+') as a:
+    userlist = a
+    if not userlist.read():
+        userlist.write("To Do List: \n")
+        userlist.seek(0)
+    print(userlist.read())
+    
+    quitter = False
+    while (quitter == False):
+        userin = input("Enter a command(add, rm, quit, print: ")
+        if userin[:3] == 'add':
+            add(userlist, userin)
+        if userin[:2] == 'rm':
+            rm(userlist, 3)
+
+
+
